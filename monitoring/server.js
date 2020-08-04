d960b4bcb5fd6e560441abe256f59a50711c9d8f
Koichi Suematsu <pascal.jp@gmail.com>
2020-08-04 17:05:52 +0900

    Temporary fix: Make the local server accessible from web page on https.

--- monitoring/server.js
+++ monitoring/server.js
@@ -24,6 +24,7 @@ class Server {
 
     this.app_.get('/getMachineId', (req, res) => {
       res.header('Content-Type', 'application/json; charset=utf-8');
+      res.header('Access-Control-Allow-Origin', 'https://brewery-app.com');
       try {
         const machineId = this.getMachineId_();
         res.json({status: 'ok', machineId: machineId});

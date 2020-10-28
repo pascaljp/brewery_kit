ee8cfe3ff80897c4d2edc8788774748678fdab13
Koichi Suematsu <pascal.jp@gmail.com>
2020-10-29 03:22:17 +0900

    Update the setup script.

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eux
 
-# Connect to wireless network.
-read -p "Input the SSID of the wireless network: " WIFI_SSID
-read -p "Input the SSID of the wireless network: " WIFI_PASSWORD
-sudo apt install -y network-manager
-nmcli device wifi connect ${WIFI_SSID} password ${WIFI_PASSWORD}
-
 # Enable Bluetooth.
 sudo apt install -y pi-bluetooth
 
@@ -18,10 +12,11 @@ sudo npm install -g pm2
 # See https://github.com/abandonware/noble/issues/93
 sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 
-sudo pm2 start inkbird.js --name inkbird
+pm2 start inkbird.js --name inkbird
 pm2 start "pm2 pull inkbird" --cron '0 * * * *' --no-autorestart
 sleep 10
-sudo pm2 save
+pm2 save
+pm2 list
 sleep 10
 
 sudo reboot

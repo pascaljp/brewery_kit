d7bf48bc22f0e67ed2ab4e23d0292417536e910b
Koichi Suematsu <pascal.jp@gmail.com>
2020-12-30 10:00:36 +0900

    do not restart the job if there is no update.

--- monitoring/restart_job.sh
+++ monitoring/restart_job.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -eux
 
+git fetch origin
+git diff origin/master
+
+if [[ "$(git fetch origin && git diff origin/master | wc -l)" == "0" ]]; then
+    echo No update.
+    exit 0
+fi
+
 git pull
 npm install
 sudo chown docker:docker /mnt/inkbird

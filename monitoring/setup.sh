e07a6dc780f2e3386f23167b8400798aa19c773d
Koichi Suematsu <pascal.jp@gmail.com>
2021-01-04 02:49:10 +0900

    Start the server during setup

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -7,9 +7,13 @@ sudo npm install n -g && sudo n stable && sudo apt-get purge -y nodejs npm
 
 # Setup nodejs's noble library.
 sudo apt install -y bluetooth bluez libbluetooth-dev libudev-dev libcap2-bin
-npm install
 sudo npm install -g pm2
 
 # Without this line node cannot read bluetooth data.
 # See https://github.com/abandonware/noble/issues/93
 sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
+
+# Start the job.
+pm2 start inkbird.js --name inkbird
+# Update the git repository and restart the job.
+./maintenance/update_job.sh

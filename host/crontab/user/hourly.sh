4fe7d5cf0f00ad81e27e337d30e16c9cbb595584
Koichi Suematsu <pascal.jp@gmail.com>
2021-02-21 07:30:15 +0900

    Move some content from crontab to a script

---
+++ host/crontab/user/hourly.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+type jq || sudo apt install -y jq
+
+ROOTDIR=$(docker volume inspect inkbird | grep Mountpoint | awk '{print $2}' | awk -F'"' '{print $2}')
+MACHINE_ID=$(sudo cat ${ROOTDIR}/config.json | jq -r .machineId)
+
+function notify() {
+    MACHINE_ID=$1
+    KEY=$2
+    DATA=$3
+    curl "https://brewery-app.com/api/client/log?machineId=${MACHINE_ID}&key=${KEY}&data=${DATA}"
+}
+
+notify ${MACHINE_ID} user.hourly started
+
+# Sync to head and restart the server if needed.
+UPDATE_RESULT=$(docker run --rm --privileged --net=host --mount type=volume,src=inkbird,dst=/mnt/inkbird pascaljp/inkbird:0.2 bash -eu brewery_kit/monitoring/maintenance/update_job.sh | tail -1)
+if [[ "${UPDATE_RESULT}" == "Updated" ]]; then
+    echo "Restart"
+    notify ${MACHINE_ID} job.restart ongoing
+    docker restart brewery-kit-instance
+    notify ${MACHINE_ID} job.restart done
+fi
+
+# Update crontab.
+cat ${ROOTDIR}/brewery_kit/master/host/crontab.user 2>/dev/null | crontab -
+notify ${MACHINE_ID} machine.crontab updated

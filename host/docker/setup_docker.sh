d2acac7699c5ff82cc15ccc497ca5957e4aedc84
Koichi Suematsu <pascal.jp@gmail.com>
2021-05-16 17:54:39 +0900

    Submit Dockerfile.

---
+++ host/docker/setup_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docker volume create inkbird
+
+# docker run --privileged --net=host --name brewery-kit-instance --mount type=volume,src=inkbird,dst=/mnt/inkbird brewery-kit /bin/bash -c "node inkbird.js --tmpdir=/mnt/inkbird" -it
+# docker stop brewery-kit-instance
+

ae73d58c7a40ce6e70dfb083c28bed2b05b961b1
Koichi Suematsu <pascal.jp@gmail.com>
2021-05-17 01:56:11 +0900

    Revert

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -eux
 
+# Setup nodejs.
+sudo apt install -y curl npm
+# Install versioned nodejs and delete the npm installed from apt.
+sudo npm install n -g && sudo n stable && sudo apt-get purge -y nodejs npm
+
+# Setup nodejs's noble library.
+sudo apt install -y bluetooth bluez libbluetooth-dev libudev-dev libcap2-bin
+sudo npm install -g pm2
+
+# Without this line node cannot read bluetooth data.
+# See https://github.com/abandonware/noble/issues/93
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
